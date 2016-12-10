031367fb2d8ced16013836be867919d213e9c18d
Varghese Chacko <varghesec@gmail.com>
2016-12-11 00:33:01 +0530

    Make dist

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='AtemonSMSAPI',
-    version='0.1',
-    packages=['SMS',],
+    version='0.1.2',
+    packages=['SMS', 'SMS/GAG'],
     license='MIT Except',
-    long_description=open('README.md').read(),
+    long_description="Connect to SMS API with Python",
 )
