19d838dc7357a1c335b88e3f460ca7bd407100e2
Varghese Chacko <varghesec@gmail.com>
2016-12-11 13:47:24 +0530

    Add README as link to README.md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='Atemon-SMSAPI',
-    version='0.1.1.0',
+    version='0.1.1.1',
     packages=['atemon', 'atemon.SMS', 'atemon.SMS.GAG'],
     long_description="Connect to SMS API with Python",
     author="Varghese Chacko",
