18ec731ad29b80ee3c4ff43e3507a6e6669c8346
Varghese Chacko <varghesec@gmail.com>
2016-12-11 08:05:17 +0530

    Change package dirs

---
+++ Atemon/__init__.py
@@ -0,0 +1,24 @@
+"""
+Various packages provided by Atemon Technology Consultants.
+
+Website: http://www.atemon.com
+Git Hub: https://github.com/atemon
+Twitter: https://twitter.com/atemonastery
+
+Author: Varghese Chacko varghese@atemon.com
+
+Copyright 2016 Atemon Technology Consultants LLP
+
+Each package may have different opensource licence(s), Please refer to LICENSE.txt
+in each package/or top of the file.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE X CONSORTIUM BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of the Atemon Technology Consultants LLP shall
+not be used in advertising or otherwise to promote the sale, use or other dealings in this
+Software without prior written authorization from the Atemon Technology Consultants LLP.
+"""
--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+"""Setup.py file for Atemon.SMS package."""
 from distutils.core import setup
 
 setup(
-    name='AtemonSMSAPI',
-    version='0.1.3',
-    packages=['SMS', 'SMS/GAG'],
-    license='MIT Except',
+    name='Atemon-SMSAPI',
+    version='0.1.0.0',
+    packages=['Atemon', 'Atemon.SMS', 'Atemon.SMS.GAG'],
     long_description="Connect to SMS API with Python",
+    author="Varghese Chacko",
+    author_email="varghese@atemon.com",
+    maintainer="Priya Mary Philip",
+    maintainer_email="priya@atemon.com",
+    url="https://github.com/atemon/python-sms-api",
+    install_requires=["requests>=2.12.3"],
+    provides=["SMS"],
+    license="MIT License (modified)",
 )
