ba77d3a26145e46edcbd450b42286d5753cae930
Varghese Chacko <varghesec@gmail.com>
2016-12-11 08:08:35 +0530

    Rename Atemon to atemon

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from distutils.core import setup
 
 setup(
     name='Atemon-SMSAPI',
-    version='0.1.0.0',
-    packages=['Atemon', 'Atemon.SMS', 'Atemon.SMS.GAG'],
+    version='0.1.0.1',
+    packages=['atemon', 'atemon.SMS', 'atemon.SMS.GAG'],
     long_description="Connect to SMS API with Python",
     author="Varghese Chacko",
     author_email="varghese@atemon.com",
-    maintainer="Priya Mary Philip",
-    maintainer_email="priya@atemon.com",
     url="https://github.com/atemon/python-sms-api",
     install_requires=["requests>=2.12.3"],
     provides=["SMS"],
